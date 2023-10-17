e8a9b90199e8e8243f6e1dbc2bcc1704f3a3caeb
wakamaujoy <wakamaujoy@gmail.com>
2023-10-17 17:52:07 +0300

    introduced images to blogposts

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 //jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -7,7 +6,8 @@ const { includes, each } = require("lodash");
 const trendNews = require(__dirname+"/f.js")
 const _ = require('lodash');
 const mongoose = require("mongoose");
-const getTrends = require("./f");
+// const getTrends = require("./f");
+
 
 const app = express();
 
@@ -21,53 +21,48 @@ mongoose.connect('mongodb://127.0.0.1:27017/blogdb')
 
 const blogSchema =new mongoose.Schema({
   title:String,
-  post:String
+  post:String,
+  date :String,
+  image: String,
 })
 const BlogPost = mongoose.model("post", blogSchema)
 
 
 app.get("/", (req, res)=>{ 
+  BlogPost.find({}, (err, respo)=>{
+    if(!err){
+      res.render("home", {posts:respo})
+    }  
+}) 
+})
+
+app.get("/posts/:mypath", (req, res)=>{   
 var url = 'https://newsapi.org/v2/everything?' +
 'q=technology&'+    
 'sortBy=popularity&' +
 'apiKey=744dc8784e464a7b8bdaddacf216f944';
-
-BlogPost.find({}, (err, respo)=>{
-  if(!err){
-    const getTrends=  async function() {
-      const resp = await fetch(url)
-      if (resp.ok) {
-        const data = await resp.json()
-        const mydata= data.articles.slice(0,5)
-        res.render("home", {posts:respo, trends:mydata})
-      }
-      }
-    
-    getTrends()
-  }
-
-  })
-  
-  
-  
-   
-})  
-
-app.get("/posts/:mypath", (req, res)=>{    
-  let nowpath =req.params.mypath
+let nowpath =req.params.mypath
   BlogPost.findOne({title:nowpath}, (err, answer)=>{
     if(!err){
-      res.render("post", {title: answer.title, body:answer.post})
+      const getTrends=  async function() {
+        const resp = await fetch(url)
+        if (resp.ok) {
+          const data = await resp.json()
+          const mydata= data.articles.slice(0,5)
+          res.render("post", {title: answer.title, body:answer.post, trends:mydata})
+        }
+        }
+      
+      getTrends()
     }
-
-  })
-  
-})  
-
-app.get("/about", (req, res)=>{
-  res.render("about")
-
+    })
 })
+
+
+// app.get("/about", (req, res)=>{
+//   res.render("about")
+
+// })
 
 app.get("/contact", (req, res)=>{
   res.render("contact")
@@ -83,7 +78,10 @@ app.post("/compose", (req, res)=>{
   const newPost = new BlogPost({
     title: req.body.newBlog,
     post: req.body.newText,
+    date:req.body.newDate,
+    image:req.body.newImage
   })
+ 
   newPost.save()
   res.redirect("/")
   
